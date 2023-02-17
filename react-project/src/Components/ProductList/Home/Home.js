3a588d9a7fd6c45b36e2f96cea9578d08b47c55d
Damian Martinez <damian@hotmail.cz>
2023-02-17 07:05:11 -0300

    feature criação de lista de produtos(provisória) e distribuição da lista através de props para gerar os cards. Contagem de produtos para mostrar ao usuário

--- react-project/src/Components/ProductList/Home/Home.js
+++ react-project/src/Components/ProductList/Home/Home.js
@@ -1,29 +1,26 @@
 import React from 'react';
 import { ProductCard } from '../ProductCard/ProductCard'
+import { Items } from '../ShoppingCart/Items/Items';
 import { HomeContainer , HomeTopContainer , QtdeProdutos , LabelListOrdenacao , DropDownListOrdenacao , DropDownOptions } from './homeStyle'
 
-export const Home = () => {
+export const Home = ({ItemsList}) => {
+  const ItemsCount = ItemsList.length;
   return (
       <HomeContainer>
         <HomeTopContainer>
-          <QtdeProdutos>Quantidade de produtos:</QtdeProdutos>
+          <QtdeProdutos>Quantidade de produtos: {ItemsCount}</QtdeProdutos>
           <LabelListOrdenacao htmlFor='ordenacao'>
             Ordenaçao:
             <DropDownListOrdenacao id='ordenacao'>
               <DropDownOptions value="crescente">Crescente</DropDownOptions>
               <DropDownOptions value="decrescente">Decrescente</DropDownOptions>
             </DropDownListOrdenacao>
-          </LabelListOrdenacao>
-
-          
+          </LabelListOrdenacao>          
         </HomeTopContainer>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
+        <ProductCard
+          ItemsList = {ItemsList}
+        />
+        
       </HomeContainer>
   )
 }
--- react-project/src/Components/ProductList/ProductCard/ProductCard.js
+++ react-project/src/Components/ProductList/ProductCard/ProductCard.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import {ProductCardContainer , ImagemProduto ,NomeProduto , ValorProduto , AddToCartButton} from './productCardStyle'
+import { CardContainer,Card , ImagemProduto ,NomeProduto , ValorProduto , AddToCartButton} from './productCardStyle'
 
-export const ProductCard = () => {
+export const ProductCard = ({ItemsList}) => {
   return (
-    <ProductCardContainer>
-      <ImagemProduto src="https://static.vecteezy.com/ti/fotos-gratis/t2/2098203-gato-malhado-prateado-sentado-no-fundo-verde-gratis-foto.jpg" alt="Foto Produto"/>
-      <NomeProduto>Nome do Produto</NomeProduto>
-      <ValorProduto>R$ 000,00</ValorProduto>
-      <AddToCartButton>Adicionar ao Carrinho</AddToCartButton>
-    </ProductCardContainer>
+    <CardContainer>
+      {ItemsList.map((produto) => (
+        <Card>
+          <ImagemProduto src={produto.imageUrl}/>
+          <NomeProduto key={produto.name}>{produto.name}</NomeProduto>
+          <ValorProduto key={produto.price}>R$ {produto.price}</ValorProduto>
+          <AddToCartButton>Adicionar ao Carrinho</AddToCartButton>
+        </Card>
+      ))}
+    </CardContainer>
   )
 }
