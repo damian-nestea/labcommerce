6ccc37f22314038a66b84f221dd2e107bfe3b77b
Damian Martinez <damian@hotmail.cz>
2023-03-14 07:10:30 -0300

    feature criação de links no footer  para navegar pelo site e estilização dos mesmos

--- react-project/src/Components/Footer/Footer.js
+++ react-project/src/Components/Footer/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import { FooterContainer } from './footerStyle'
+import { FooterContainer , AstroDevLinks , Links} from './footerStyle'
 
 export const Footer = () => {
   return (
     <FooterContainer>
-      
+      <AstroDevLinks>
+        ASTRODEV LINKS
+        <li><Links href="./">Início</Links></li>
+        <li><Links href="./">Shop</Links></li>
+        <li><Links href="./">Sobre nós</Links></li>
+        <li><Links href="./">Trabalhe conosco</Links></li>
+        
+      </AstroDevLinks>
     </FooterContainer>
   )
 }
--- react-project/src/Components/Footer/footerStyle.js
+++ react-project/src/Components/Footer/footerStyle.js
@@ -1,6 +1,39 @@
 import styled from 'styled-components'
 
 export const FooterContainer = styled.footer`
-  height:30vh;
+  min-height:30vh;
   background-color:#00665e;
+  font-family:'Sono', sans-serif;
+  display:flex;
+  padding: 5rem 10rem;
+`
+
+export const AstroDevLinks = styled.ul`
+  list-style-type:none;
+  display:flex;
+  flex-direction:column;
+  gap:0.2rem;
+  color:#FFF;
+  font-weight:700;
+`
+export const Links = styled.a`
+  :link , :visited{
+    text-decoration:none;
+    text-transform:uppercase;
+    font-weight:400;
+    color:#FFF;
+  }
+  ::before{
+    content:" ";
+    display:inline-block;
+    vertical-align:middle;
+    width:0.5rem;
+    height:0.4rem;
+    background-color:#F68A53;
+    margin-right:0.5rem;
+    border-radius:100%;
+  }
+  :hover{
+    color:#F68A53
+  }
 `
