3eea7dcb6d19a1792e97eb4e6db3194a1196d64e
Damian Martinez <damian@hotmail.cz>
2023-03-16 06:22:44 -0300

    feature adição de links no footer , seção de links úteis e contato, adição de fonte extra light na aplicação

--- react-project/src/Components/Footer/Footer.js
+++ react-project/src/Components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FooterContainer , AstroDevLinks , Links} from './footerStyle'
+import { FooterContainer , AstroDevLinks , Links , ContatoLinks} from './footerStyle'
 
 export const Footer = () => {
   return (
@@ -10,7 +10,22 @@ export const Footer = () => {
         <li><Links href="./">Shop</Links></li>
         <li><Links href="./">Sobre nós</Links></li>
         <li><Links href="./">Trabalhe conosco</Links></li>
-        
+      </AstroDevLinks>
+      <AstroDevLinks>
+        LINKS ÚTEIS
+        <li><Links href="./">Localizador de  lojas</Links></li>
+        <li><Links href="./">Perguntas frequentes</Links></li>
+        <li><Links href="./">Área do cliente</Links></li>
+        <li><Links href="./">Política de privacidade</Links></li>
+        <li><Links href="./">Termos e condições</Links></li>
+      </AstroDevLinks>
+      <AstroDevLinks>
+        CONTATO
+        <li><ContatoLinks href="./">SHOP: hello@astrodevshop.com</ContatoLinks></li>
+        <li><ContatoLinks href="./">IMPRENSA: press@astrodevshop.com</ContatoLinks></li>
+        <li><ContatoLinks href="./">Área do cliente</ContatoLinks></li>
+        <li><ContatoLinks href="./">Política de privacidade</ContatoLinks></li>
+        <li><ContatoLinks href="./">Termos e condições</ContatoLinks></li>
       </AstroDevLinks>
     </FooterContainer>
   )
--- react-project/src/Components/Footer/footerStyle.js
+++ react-project/src/Components/Footer/footerStyle.js
@@ -5,6 +5,7 @@ export const FooterContainer = styled.footer`
   background-color:#00665e;
   font-family:'Sono', sans-serif;
   display:flex;
+  justify-content:space-between;
   padding: 5rem 10rem;
 `
 
@@ -20,7 +21,28 @@ export const Links = styled.a`
   :link , :visited{
     text-decoration:none;
     text-transform:uppercase;
-    font-weight:400;
+    font-weight:200;
+    color:#FFF;
+  }
+  ::before{
+    content:" ";
+    display:inline-block;
+    vertical-align:middle;
+    width:0.5rem;
+    height:0.4rem;
+    background-color:#F68A53;
+    margin-right:0.5rem;
+    border-radius:100%;
+  }
+  :hover{
+    color:#F68A53
+  }
+`
+
+export const ContatoLinks = styled.a`
+  :link , :visited{
+    text-decoration:none;
+    font-weight:200;
     color:#FFF;
   }
   ::before{
