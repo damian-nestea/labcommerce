f9cedb4878817e1604fa17a4f020e156d6ed77b9
damian-nestea <damian@hotmail.cz>
2023-03-21 12:47:00 -0300

    feature adição de icone para add to cart no projeto

--- react-project/src/Components/ProductList/ProductCard/ProductCard.js
+++ react-project/src/Components/ProductList/ProductCard/ProductCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card , ContainerImagemProduto , ImagemProduto , NomeProduto , DescricaoProduto , ValorProduto , AddToCartButton} from './productCardStyle'
 import { FormatPrice } from '../FormatPrice/FormatPrice'
+import AddToCart from '.../assets/img/add-to-cart.svg'
 
 export const ProductCard = ({product, addToCart}) => {
   return (
@@ -11,7 +12,7 @@ export const ProductCard = ({product, addToCart}) => {
           <NomeProduto key={product.name}>{product.name}</NomeProduto>
           <DescricaoProduto key={product.description}>{product.description}</DescricaoProduto>
           <ValorProduto key={product.price}><FormatPrice price = {product.price} /> </ValorProduto>
-          <AddToCartButton onClick={()=>addToCart(product)}>Adicionar ao Carrinho</AddToCartButton>
+          <AddToCartButton onClick={()=>addToCart(product)}><img src={AddToCart} alt='Icone adicionar no carrinho'/></AddToCartButton>
         </Card>
   )
 }
