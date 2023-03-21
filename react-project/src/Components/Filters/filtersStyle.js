cbf0f46ba0362833674b34f1582f5d2770300dee
damian-nestea <damian@hotmail.cz>
2023-03-20 21:33:21 -0300

    feature estilização dos inputs dos filtros

--- react-project/src/Components/Filters/filtersStyle.js
+++ react-project/src/Components/Filters/filtersStyle.js
@@ -10,6 +10,7 @@ export const FiltersContainer = styled.section`
 `
 
 export const FiltersTitle = styled.h2`
+color:#00665e;
 
 `
 
@@ -25,9 +26,20 @@ export const StyledLabel = styled.label`
   display:flex;
   flex-direction:column;
   gap:0.1rem;
+  color:#00665e;
 `
 
 export const Input = styled.input`
+  :focus{
+    outline:none;
+  }
+  font-family:'Sono', sans-serif;
+  color:#009A87;
+  font-size:1rem;
+  height:2rem;
+  padding:0.2rem;
+  border:0;
+  border-bottom:0.05rem solid #009A87;
   ::-webkit-outer-spin-button,
   ::-webkit-inner-spin-button {
     -webkit-appearance: none;
