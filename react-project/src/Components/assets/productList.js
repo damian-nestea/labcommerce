f28243802d7ced00f0966e49b41efaf41cff423b
damian-nestea <damian@hotmail.cz>
2023-03-23 21:09:05 -0300

    refactor adição de fotos editadas e tratadas para mostrar no card de produto

--- react-project/src/Components/assets/productList.js
+++ react-project/src/Components/assets/productList.js
@@ -1,11 +1,11 @@
 import astronautHelmet from './img/productImg/astronautHelmet01.png'
-import astronautIsolated01 from './img/productImg/spaceSuit01.png'
-import astronautIsolated02 from './img/productImg/spaceSuit02.png'
-import astronautIsolated03 from './img/productImg/spaceSuit03.png'
-import astronautIsolated04 from './img/productImg/spaceSuit04.png'
-import astronautIsolated05 from './img/productImg/spaceSuit05.png'
-import childrenAstronautIsolated01 from './img/productImg/childrenAstronautIsolated01.png'
-import childrenAstronautIsolated02 from './img/productImg/childrenAstronautIsolated02.png'
+import spaceSuit01 from './img/productImg/spaceSuit01.png'
+import spaceSuit02 from './img/productImg/spaceSuit02.png'
+import spaceSuit03 from './img/productImg/spaceSuit03.png'
+import spaceSuit04 from './img/productImg/spaceSuit04.png'
+import spaceSuit05 from './img/productImg/spaceSuit05.png'
+import childSpaceSuit01 from './img/productImg/childSpaceSuit01.png'
+import childSpaceSuit02 from './img/productImg/childSpaceSuit02.png'
 import decoration01 from './img/productImg/decoration01.png'
 import satellite01 from './img/productImg/satellite01.png'
 import satellite02 from './img/productImg/satellite02.png'
@@ -23,49 +23,49 @@ export const ItemsList = [
     name: "Traje Espacial T1",
     description: "EMU espacial de última geração e alta eficiência.",
     price: 5000,
-    imageUrl: astronautIsolated01
+    imageUrl: spaceSuit01
   },
   {
     id:3,
     name: "Traje Espacial T2",
     description: "EMU espacial de resistência a condições extremas.",
     price: 4500,
-    imageUrl: astronautIsolated02
+    imageUrl: spaceSuit02
   },
   {
     id:4,
     name: "Traje Espacial T3",
     description: "EMU espacial de alta proteção e mobilidade otimizada.",
     price: 4000,
-    imageUrl: astronautIsolated03
+    imageUrl: spaceSuit03
   },
   {
     id:5,
     name: "Traje Espacial T4",
     description: "EMU espacial para astronautas exigentes. Resistência máxima",
     price: 5800,
-    imageUrl: astronautIsolated04
+    imageUrl: spaceSuit04
   },
   {
     id:6,
     name: "Traje Espacial T5",
     description: "EMU espacial de alta performance para testes em terra",
     price: 3700,
-    imageUrl: astronautIsolated05
+    imageUrl: spaceSuit05
   },
   {
     id:7,
     name: "EMU Espacial Kids K1",
     description: "EMU espacial infantil criado para testes em terra.",
     price: 4100,
-    imageUrl: childrenAstronautIsolated01
+    imageUrl: childSpaceSuit01
   },
   {
     id:8,
     name: "EMU Espacial Kids K2",
     description: "EMU espacial infantil de alta performance e resistência.",
     price: 4300,
-    imageUrl: childrenAstronautIsolated02
+    imageUrl: childSpaceSuit02
   },
   {
     id:9,
