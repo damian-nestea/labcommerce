ceba0b3ec9f318c3d354dfad8e7a31d7a49740b2
damian-nestea <damian@hotmail.cz>
2023-06-27 15:39:21 -0300

    feat responsividade para tela até 1400px do  productList

--- react-project/src/Components/ProductList/ShoppingCart/Cart/cartStyle.js
+++ react-project/src/Components/ProductList/ShoppingCart/Cart/cartStyle.js
@@ -1,39 +1,42 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const CartContainer = styled.section`
-  width:20vw;
-  min-height:80vh;
-  display:flex;
-  flex-direction:column;
-  padding:1rem;
-`
+  width: 20vw;
+  min-height: 80vh;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  @media screen and (max-width: 1400px) {
+    display: none;
+  }
+`;
 export const ItemsContainer = styled.article`
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-  gap:0.5rem;
-  margin:2rem 0;
-  padding:1rem;
-  color:#00665e;
-  font-size:1rem;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  margin: 2rem 0;
+  padding: 1rem;
+  color: #00665e;
+  font-size: 1rem;
+`;
 export const CartTitle = styled.h2`
-  color:#00665e;
-`
+  color: #00665e;
+`;
 export const ValorTotal = styled.p`
-  font-size:0.8rem;
-  color:#009A87;
-`
+  font-size: 0.8rem;
+  color: #009a87;
+`;
 export const FinalizarCompra = styled.button`
-  margin-top:2rem;
-  background-color:#f68a53;
+  margin-top: 2rem;
+  background-color: #f68a53;
   color: #fff;
-  text-transform:uppercase;
-  font-family:'Sono' , sans-serif;
-  font-size:1.1rem;
-  font-weight:500;
-  padding:0.5rem;
-  border:none;
-  border-radius:0.2rem;
-  cursor:pointer;
-`
+  text-transform: uppercase;
+  font-family: "Sono", sans-serif;
+  font-size: 1.1rem;
+  font-weight: 500;
+  padding: 0.5rem;
+  border: none;
+  border-radius: 0.2rem;
+  cursor: pointer;
+`;
