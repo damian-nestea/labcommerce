d76aa7f76b5dad5f04398c005d46b7d049c49bf7
damian-nestea <damian@hotmail.cz>
2024-01-02 17:32:18 -0300

    feat adição de media query no footer para telas mobile

--- react-project/src/Components/Footer/footerStyle.js
+++ react-project/src/Components/Footer/footerStyle.js
@@ -1,95 +1,118 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const FooterMainContainer = styled.footer`
-  min-height:30vh;
-  background-color:#00665e;
-  font-family:'Sono', sans-serif;
-  display:flex;
-  flex-direction:column;
-  gap:5rem;
+  min-height: 30vh;
+  background-color: #00665e;
+  font-family: "Sono", sans-serif;
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
   padding: 5rem 10rem;
-`
+
+  @media screen and (max-width: 800px) {
+    padding: 2rem;
+  }
+`;
 export const FooterTopContainer = styled.section`
-  display:flex;
-  justify-content:space-between;
-`
+  display: flex;
+  justify-content: space-between;
+
+  @media screen and (max-width: 1300px) {
+    gap: 2rem;
+    flex-wrap: wrap;
+  }
+
+  @media screen and (max-width: 800px) {
+    gap: 2rem;
+    flex-direction: column;
+  }
+`;
 export const AstroDevLinks = styled.ul`
-  list-style-type:none;
-  display:flex;
-  flex-direction:column;
-  gap:0.2rem;
-  color:#FFF;
-  font-weight:700;
-`
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  gap: 0.2rem;
+  color: #fff;
+  font-weight: 700;
+`;
 export const Links = styled.a`
-  :link , :visited{
-    text-decoration:none;
-    text-transform:uppercase;
-    font-weight:200;
-    color:#FFF;
+  :link,
+  :visited {
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: 200;
+    color: #fff;
     transition: 0.5s;
   }
-  ::before{
-    content:" ";
-    display:inline-block;
-    vertical-align:middle;
-    width:0.5rem;
-    height:0.4rem;
-    background-color:#F68A53;
-    margin-right:0.5rem;
-    border-radius:100%;
+  ::before {
+    content: " ";
+    display: inline-block;
+    vertical-align: middle;
+    width: 0.5rem;
+    height: 0.4rem;
+    background-color: #f68a53;
+    margin-right: 0.5rem;
+    border-radius: 100%;
   }
-  :hover{
-    color:#F68A53
+  :hover {
+    color: #f68a53;
   }
-`
+`;
 export const ContatoLinks = styled.a`
-  :link , :visited{
-    text-decoration:none;
-    font-weight:200;
-    color:#FFF;
+  :link,
+  :visited {
+    text-decoration: none;
+    font-weight: 200;
+    color: #fff;
     transition: 0.5s;
   }
-  ::before{
-    content:" ";
-    display:inline-block;
-    vertical-align:middle;
-    width:0.5rem;
-    height:0.4rem;
-    background-color:#F68A53;
-    margin-right:0.5rem;
-    border-radius:100%;
+  ::before {
+    content: " ";
+    display: inline-block;
+    vertical-align: middle;
+    width: 0.5rem;
+    height: 0.4rem;
+    background-color: #f68a53;
+    margin-right: 0.5rem;
+    border-radius: 100%;
   }
-  :hover{
-    color:#F68A53
+  :hover {
+    color: #f68a53;
   }
-`
+`;
 export const Underline = styled.span`
-  text-decoration:underline;
-  text-decoration-color:#F68A53;
-`
+  text-decoration: underline;
+  text-decoration-color: #f68a53;
+`;
 export const FooterBottomContainer = styled.section`
-  min-height:3rem;
-  display:flex;
-  justify-content:space-between;
-  align-items:center;
-`
+  min-height: 3rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  @media screen and (max-width: 800px) {
+    gap: 2rem;
+    flex-direction: column;
+  }
+`;
 export const DireitosReservados = styled.p`
-  font-size:0.8rem;
-  color:#A2A2A2;
-`
+  font-size: 0.8rem;
+  color: #a2a2a2;
+  @media screen and (max-width: 800px) {
+    text-align: center;
+  }
+`;
 export const AstroDevRedesSociais = styled.ul`
-  list-style-type:none;
-  display:flex;
-  gap:0.5rem;
-`
+  list-style-type: none;
+  display: flex;
+  gap: 0.5rem;
+`;
 export const LinksRedesSociais = styled.a`
-  img{
-    width:2rem;
-    border-radius:0.5rem;
+  img {
+    width: 2rem;
+    border-radius: 0.5rem;
     transition: 0.3s;
-    :hover{
-      width:2.2rem;
+    :hover {
+      width: 2.2rem;
     }
   }
-`
+`;
